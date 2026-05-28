d6c934b705cec14a0270dc1920767c279a7770c8
Lucas J Santos <lucksander@outlook.com.br>
2026-05-27 23:23:48 -0300

    Atualizei
    Coloquei Imagens

---
+++ Ola Mundo Personalizado.py
@@ -0,0 +1,3 @@
+# Pede o nome do usuário e exibe uma mensagem personalizada de boas-vindas
+Nome = input("Qual é o seu Nome?")
+print(f"Olá, {Nome}, Seja Bem Vindo(a) Ao Mundo Python🚀!")
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Projeto simples em Python criado para praticar programação e iniciar portfólio no GitHub.
 
+## Preview do projeto
+
+![Preview](assets/exemplo2.png)
